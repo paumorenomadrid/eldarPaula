3a475f81c7fbdf0febbf6e9415b622bea5a35444
paula <paulamorenomadrid@gmail.com>
2023-08-04 17:12:37 -0300

    project

---
+++ src/main/java/com/example/eldar/EldarPaulaApplication.java
@@ -0,0 +1,15 @@
+package com.example.eldar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.eldar.model.CreditCard;
+
+@SpringBootApplication
+public class EldarPaulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EldarPaulaApplication.class, args);
+	}
+
+}
